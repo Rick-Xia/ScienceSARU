dba4da32f82653bef9f0f148fd6c4651be7dfe98
Rick-Xia <danteveqann@gmail.com>
2018-04-06 16:36:54 -0400

    basic setup

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const tokenfile = require('./token.json');
+const botconfig = require('./botconfig.json');
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity("Crusader Kings II");
+});
+
+bot.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm" ) return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}meigen`) {
+        return message.channel.send("空にある星がすべてと思う勿れ！");
+    }
+
+    if (cmd === `${prefix}botinfo`) {
+        let bicon = bot.user.displayAvatarURL;
+
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#15f153")
+        .setThumbnail(bicon)
+        .addField("Bot Name", bot.user.username)
+        .addField("Created On", bot.user.createdAt);
+
+        return message.channel.send(botembed);
+    }
+});
+
+bot.login(tokenfile.token);
