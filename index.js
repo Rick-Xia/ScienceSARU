fb657c3bf11fc98495a06035c85d810c6058a758
Rick-Xia <danteveqann@gmail.com>
2018-04-06 17:55:31 -0400

    revised meigen system

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const botconfig = require('./botconfig.json');
 
 const bot = new Discord.Client({disableEveryone: true});
 
+const errorMsg = "It's so busy here...I need money for the Antarctica anyway!";
+
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
     bot.user.setActivity("Crusader Kings II");
@@ -18,32 +20,37 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    if (cmd === `${prefix}meigen`) {
-        const meigenCollection = require('./meigen.json');
+    if (cmd.charAt(0) === prefix) {
+        let cmdContent = cmd.substring(1);
 
-        let id = Math.floor(Math.random() * 5).toString();
-        let meigen = meigenCollection[id];
-        let meigenembed = new Discord.RichEmbed()
-        .setDescription("名言" + meigen.id)
-        .setColor("#ff9933")
-        .setThumbnail(bot.user.displayAvatarURL)
-        .addField("Line", meigen.line)
-        .addField("Translation(CHN)", meigen.trans);
+        if (cmdContent === `meigen`) {
+            const meigenCollection = require('./meigen.json');
 
-        return message.channel.send(meigenembed);
-    }
+            let id = Math.floor(Math.random() * 5).toString();
+            let meigen = meigenCollection[id];
+            let meigenembed = new Discord.RichEmbed()
+            .setDescription("名言" + meigen.id)
+            .setColor("#ff9933")
+            .setThumbnail(bot.user.displayAvatarURL)
+            .addField("Line", meigen.line)
+            .addField("Translation(CHN)", meigen.trans);
 
-    if (cmd === `${prefix}botinfo`) {
-        let bicon = bot.user.displayAvatarURL;
+            return message.channel.send(meigenembed);
+        }
+        else if (cmdContent === `botinfo`) {
+            let bicon = bot.user.displayAvatarURL;
 
-        let botembed = new Discord.RichEmbed()
-        .setDescription("Bot Information")
-        .setColor("#ff9933")
-        .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username)
-        .addField("Created On", bot.user.createdAt);
+            let botembed = new Discord.RichEmbed()
+            .setDescription("Bot Information")
+            .setColor("#ff9933")
+            .setThumbnail(bicon)
+            .addField("Bot Name", bot.user.username)
+            .addField("Created On", bot.user.createdAt);
 
-        return message.channel.send(botembed);
+            return message.channel.send(botembed);
+        }
+
+        return message.channel.send(errorMsg + "\n(Bot didn't seem to understand your command)");
     }
 });
 
