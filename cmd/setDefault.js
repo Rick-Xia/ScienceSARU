e4545a945e6b63e4ae378d287c1388d62f33a3b8
Rick-Xia <danteveqann@gmail.com>
2018-04-26 21:20:58 -0400

    r6stats: setup database for local user

---
+++ cmd/setDefault.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+var USERS;
+fs.readFile('../localdata/r6users.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    USERS = JSON.parse(data);
+});
+
+module.exports.run = async (bot, message, args) => {
+    console.log(`the author id is ${message.author.id}`);
+    console.log(`the author id is ${message.author.username}`);
+
+    let id = args[0];
+    
+}
+
+module.exports.help = {
+	name: "setDefault",
+	cmd: "setDefault"
+}
