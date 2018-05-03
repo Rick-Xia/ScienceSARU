b1f0e3683017ea662d5ec64998f28f08af6552e1
Rick-Xia <danteveqann@gmail.com>
2018-05-03 15:28:42 -0400

    dbsearch: added callback to unify style

--- bin/dbSearch.js
+++ bin/dbSearch.js
@@ -3,24 +3,25 @@ const R6USERFILENAME = "./localdata/r6users.json";
 
 let USERSFILE = fs.readFileSync(R6USERFILENAME), USERS = JSON.parse(USERSFILE);
 
-module.exports.get = ( id ) => {
-    return USERS.hasOwnProperty(id)? USERS[id] : "";
+module.exports.get = ( id, next ) => {
+    if ( USERS.hasOwnProperty(id) ) next(USERS[id]);
+    else next();
 }
 
-module.exports.post = async ( id, val ) => {
+module.exports.post = async ( id, val, next ) => {
     USERS[id] = val;
     fs.writeFile(R6USERFILENAME, JSON.stringify(USERS, null, 2), (err) => { if (err) throw err; });
-    return;
+    next();
 }
 
-module.exports.put = async ( id, val ) => {
+module.exports.put = async ( id, val, next ) => {
     USERS[id] = val;
     fs.writeFile(R6USERFILENAME, JSON.stringify(USERS, null, 2), (err) => { if (err) throw err; });
-    return;
+    next();
 }
 
-module.exports.delete = async ( id ) => {
+module.exports.delete = async ( id, next ) => {
     delete USERS[id];
     fs.writeFile(R6USERFILENAME, JSON.stringify(USERS, null, 2), (err) => { if (err) throw err; });
-    return;
+    next();
 }
